393e2d6be5bb0a95a04a4b916574c71ae7ce6349
chenjiefeng <1497735430@qq.com>
2026-05-22 20:49:38 +0800

    feat: 添加控制台输出为 UTF-8 的设置以避免中文乱码

--- sandbox/src/InputTest/main.cpp
+++ sandbox/src/InputTest/main.cpp
@@ -1,7 +1,18 @@
 #include <Engine/OpenGL/OpenGLGraphicsFactory.h>
 #include "InputTestApp.h"
+#include <clocale>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main() {
+    // 设置控制台输出为 UTF-8，避免中文乱码
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    std::setlocale(LC_ALL, "en_US.UTF-8");
+
     Engine::OpenGLGraphicsFactory factory;
     Engine::InputTestApp app(factory);
     app.Run();
